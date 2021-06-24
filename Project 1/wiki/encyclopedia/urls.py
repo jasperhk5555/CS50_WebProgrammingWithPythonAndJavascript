f359a969199cdec3860668f4206891e62ea2b445
Jasper Cheng <chengjasper@gmail.com>
2021-06-24 03:12:11 -0700

    completing project 1

--- Project 1/wiki/encyclopedia/urls.py
+++ Project 1/wiki/encyclopedia/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:name>", views.page, name="page"),
+    path("search", views.search, name="search"),
+    path("create", views.create, name="create"),
+    path("upload", views.upload, name="upload"),
+    path("existingpage", views.existing, name="existingpage"),
+    path("wiki/edit/<str:name>", views.edit, name="edit"),
+    path("random", views.random, name="random"),
 ]
--- Project 1/wiki/encyclopedia/views.py
+++ Project 1/wiki/encyclopedia/views.py
@@ -1,10 +1,76 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from . import util
+
+import markdown
+import secrets
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def page(request, name):
+    entry = util.get_entry(name)
+    if entry is None:
+        return render(request, "encyclopedia/nopage.html", {
+        "name": name
+        })
+    else:
+        return render(request, "encyclopedia/page.html", {
+            "name": name, "link":"{}.md".format(name), 
+            "content": markdown.markdown(entry)
+    })
+
+def search(request):
+    value = request.GET.get('q')
+    suggestions = []
+    if util.get_entry(value) is None:
+        for i in util.list_entries():
+            if value.upper() in i.upper():
+                suggestions.append(i)
+        return render(request, "encyclopedia/index.html", {
+            "entries": suggestions
+            })
+    else:
+        return HttpResponseRedirect(f'/wiki/{value}')
+  
+def create(request):
+    return render(request, "encyclopedia/create.html")
+
+def upload(request):
+    value = request.POST.get('q')
+    content = request.POST.get('c')
+    action = request.POST.get('a')
+    if action == "edit":
+        file = open(f"entries/{value}.md", 'w+')
+        file.write(f"{content}")
+        file.close()
+        return HttpResponseRedirect(f'/wiki/{value}')
+    elif util.get_entry(value) is None:
+        file = open(f"entries/{value}.md", 'w')
+        file.write(f"#{value}\n")
+        file.write(f"{content}")
+        file.close()
+        return HttpResponseRedirect(f'/wiki/{value}')
+    else:
+        return HttpResponseRedirect(f'/existingpage')
+        
+def existing(request):
+    return render(request, "encyclopedia/existingpage")
+
+def edit(request, name):
+    file = open(f"entries/{name}.md","r")
+    lines = file.read()
+    file.close()
+    return render(request, "encyclopedia/edit.html", {
+        "name":name, "content":lines
+    })
+
+def random(request):
+    l = util.list_entries()
+    choice = secrets.choice(l)
+    return HttpResponseRedirect(f"/wiki/{choice}")
 
